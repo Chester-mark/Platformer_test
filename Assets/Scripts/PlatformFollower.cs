499df2ae79f93c43ea1b3c8394895408bfa7089b
Chester-mark <56479743+Chester-mark@users.noreply.github.com>
2024-07-01 16:13:21 +0800

    Added a moving platform
    Added moving platform function

---
+++ Assets/Scripts/PlatformFollower.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoint;
+    private int currentIndex = 0;
+
+    [SerializeField] private float speed = 4f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(waypoint[currentIndex].transform.position, transform.position) < .1f)
+        {
+            currentIndex++;
+
+            if (currentIndex >= waypoint.Length) { 
+                currentIndex = 0;
+            }
+            
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoint[currentIndex].transform.position, Time.deltaTime * speed);
+    }
+}
